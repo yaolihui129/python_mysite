71fb579d7568ca848fee6eafebde54ab3cfab926
yaolihui129 <yaolihui129@sohu.com>
2017-10-11 16:09:57 +0800

    添加article的模型和admin页面

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Article
+from .models import Author
+from .models import Tag
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'Author', 'Tags',)
+
+admin.site.register(Article)
+admin.site.register(Author)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,35 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    qq = models.CharField(max_length=10)
+    addr = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author)
+    content = models.TextField()
+    score = models.IntegerField()  # 文章的打分
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
+
+
+@python_2_unicode_compatible
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
